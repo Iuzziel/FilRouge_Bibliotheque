2e5887fee42346e65b68e06db28db0542466ea17
Iuzziel <Iuzziel@users.noreply.github.com>
2017-07-13 14:46:51 +0200

    Update Principale.java

--- src/application/Principale.java
+++ src/application/Principale.java
@@ -10,13 +10,14 @@ public class Principale {
 		FenetreClient fenetre = new FenetreClient();
 		
 		GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment(); 
-		Rectangle rec = ge.getMaximumWindowBounds();
+		Rectangle tailleEcran = ge.getMaximumWindowBounds();
 		
-		fenetre.setSize(rec.getSize());
-		
+		fenetre.setSize(tailleEcran.getSize());
 		fenetre.setVisible(true);
-		/*FenetreEmploye fenetreEmploye = new FenetreEmploye();
-		fenetreEmploye.setVisible(true);*/
+		
+		FenetreEmploye fenetreEmploye = new FenetreEmploye();
+		fenetreEmploye.setSize(tailleEcran.getSize());
+		fenetreEmploye.setVisible(true);
 	}
 
 }
