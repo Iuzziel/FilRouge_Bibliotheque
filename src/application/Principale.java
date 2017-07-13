f2e4fa23e4c8d51c15141f52067f68d645b3ecf0
Iuzziel <Iuzziel@users.noreply.github.com>
2017-07-13 14:30:45 +0200

    Recuperation taille jwindows depuis taille ecran

--- src/application/Principale.java
+++ src/application/Principale.java
@@ -1,14 +1,22 @@
 package application;
 
+import java.awt.GraphicsEnvironment;
+import java.awt.Rectangle;
 import fenetres.*;
 
 public class Principale {
-	// *********************************Debut de la Main*********************************//
-	public static void main(String[] args) {
+	//*********************************Début de la Main*********************************//
+	public static void main (String [] args){
 		FenetreClient fenetre = new FenetreClient();
+		
+		GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment(); 
+		Rectangle rec = ge.getMaximumWindowBounds();
+		
+		fenetre.setSize(rec.getSize());
+		
 		fenetre.setVisible(true);
-		FenetreEmploye fenetreEmploye = new FenetreEmploye();
-		fenetreEmploye.setVisible(true);
+		/*FenetreEmploye fenetreEmploye = new FenetreEmploye();
+		fenetreEmploye.setVisible(true);*/
 	}
 
 }
